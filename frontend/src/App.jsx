0f3e748b0aaf17a78b59417d93bb2a976f050b15
Misiker_Genene <146325517+Misikerr@users.noreply.github.com>
2025-10-09 15:46:35 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import About from './pages/About'
 import Collection from './pages/Collection'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
+import DiscountBanner from "./components/DiscountBanner";
 import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
@@ -57,6 +58,7 @@ const App = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/place-order' element={<PlaceOrder/>}/>
         <Route path='/orders' element={<Orders/>}/>
+        <Route path="/discount" element={<DiscountBanner />} />
       </Routes>
       <Footer/>
     </div>
